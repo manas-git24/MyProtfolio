b274e890906fb47a521869e3a0635e5dedb32cb7
Your Name <manaspanwar24@gmail.com>
2025-09-14 20:04:48 +0530

    feat(header): added icons transition corrected spacing

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -1,15 +1,27 @@
 import React from 'react'
+import { FaLinkedin, FaGithub, FaEnvelope } from 'react-icons/fa'
 
 const Header = () => {
     return (
-        <header className="bg-white shadow-md p-4 flex justify-between items-center">
-            <h1 className="text-black text-4xl font-bold">Manas P.</h1>
-            <nav className="space-x-5">
-                <a href="#home" className="text-gray-700 hover:text-blue-700">Home</a>
-                <a href="#about" className="text-gray-700 hover:text-blue-700">About</a>
-                <a href="#projects" className="text-gray-700 hover:text-blue-700">Projects</a>
-                <a href="#contact" className="text-gray-700 hover:text-blue-700">Contact Me</a>
-            </nav>
+        <header className='fixed bg-white shadow-md w-full'>
+            <div className='flex justify-between items-center px-6 py-4'>
+                <h1 className='text-black text-4xl font-serif'>Manas P.</h1>
+                <nav className='space-x-6  hidden md:flex'>
+                    {/* Text links */}
+                    <a href='#home' className='text-gray-700 hover:text-blue-700 transition transform hover:scale-110'>Home</a>
+                    <a href='#about' className='text-gray-700 hover:text-blue-700 transition transform hover:scale-110'>About Me</a>
+                    <a href='#projects' className='text-gray-700 hover:text-blue-700 transition transform hover:scale-110'>Projects</a>
+                    {/* <a href='#contact' className='text-gray-700 hover:text-blue-700'>Contact Me</a> */}
+                    <a href='#resume' className='text-gray-700 hover:text-blue-700 transition transform hover:scale-110'>Resume</a>
+                    {/* Icon links */}
+                    <a href='https://www.linkedin.com/in/manas-panwar-05343b252/' target='_blank' rel='noopener noreferrer'
+                        className='text-gray-700 hover:text-blue-700 text-xl transition transform hover:scale-110 inline-flex items-center'><FaLinkedin /></a>
+                    <a href='https://github.com/manas-git24' target='_blank' rel='noopener noreferrer'
+                        className='text-gray-700 hover:text-blue-700 text-xl transition transform hover:scale-110 inline-flex items-center'><FaGithub /></a>
+                    <a href='mailto:manaspanwar24@gmail.com' rel='noopener noreferrer'
+                        className='text-gray-700 hover:text-blue-700 text-xl transition transform hover:scale-110 inline-flex items-center'><FaEnvelope /></a>
+                </nav>
+            </div>
         </header>
     )
 }
