9f5eecd0b5aa1bf5a6a0ffbd4dadc25933bc47a5
Your Name <manaspanwar24@gmail.com>
2025-09-14 17:23:14 +0530

    prototype

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Header = () => {
     return (
         <header className="bg-white shadow-md p-4 flex justify-between items-center">
-            <h1 className="text-black text-4xl font-bold">My Portfolio</h1>
+            <h1 className="text-black text-4xl font-bold">Manas P.</h1>
             <nav className="space-x-5">
                 <a href="#home" className="text-gray-700 hover:text-blue-700">Home</a>
                 <a href="#about" className="text-gray-700 hover:text-blue-700">About</a>
--- src/app/components/ProjectsPage.jsx
+++ src/app/components/ProjectsPage.jsx
@@ -4,16 +4,7 @@ const ProjectsPage = () => {
   return (
     <section id="projects" className="py-16 bg-gray-100">
       <h2 className="text-3xl text-black font-bold text-center mb-10">Projects</h2>
-      <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8 px-8">
-        <div className="bg-white shadow-md rounded-lg p-6 hover:shadow-xl transition">
-          <h3 className="text-xl font-semibold mb-2">Project 1</h3>
-          <p className="text-gray-700">Description of project 1.</p>
-        </div>
-        <div className="bg-white shadow-md rounded-lg p-6 hover:shadow-xl transition">
-          <h3 className="text-xl font-semibold mb-2">Project 2</h3>
-          <p className="text-gray-700">Description of project 2.</p>
-        </div>
-      </div>
+      
     </section>
   )
 }
